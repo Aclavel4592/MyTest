596027a5359e1b3cd1338f7748260b0efec67298
antoineclavel <clavel.antoine@gmail.com>
2025-02-18 23:56:37 +0100

    nouveau-cv

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,15 @@ class HomeController < ApplicationController
   def index
   end
 
+  def french
+  end
+
   def english
   end
 
   def spanish
+  end
+
+  def project
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/english', to: 'home#english'
   get 'home/spanish', to: 'home#spanish'
+  get 'home/french', to: 'home#french'
+  get 'home/project', to: 'home#project'
 end
